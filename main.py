87356d9a8f7f6d946ac796e6471056b16402e8ba
VyacheslavMitin <vyacheslav.mitin@gmail.com>
2023-04-14 17:51:46 +0400

    переписал без выделения 10 штук, удаляет по одному

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import pyautogui as pg
 import time
 import keyboard
 
+quantitas = 3487  # количество раз
 
-time.sleep(3)  # таймаут для на переключение в аутлук
+time.sleep(5)  # таймаут для на переключение в аутлук
+print("Старт начала работы скрипта по удалению писем")
 
-
-for i in range (100):
-    keyboard.press('shift')
-    time.sleep(0.5)
-    pg.press('down', presses=10)
-    time.sleep(0.5)
-    keyboard.release('shift')
-    time.sleep(0.5)
-    pg.press('delete')
-    time.sleep(0.5)
+for i in range (quantitas):
+    # keyboard.press('shift')
+    # time.sleep(0.5)
+    # pg.press('down', presses=10)
+    # time.sleep(0.5)
+    # keyboard.release('shift')
+    # time.sleep(0.5)
+    pg.press ('delete')
+    # time.sleep(0.1)
     keyboard.press_and_release('shift', 'tab')
     # pg.hotkey('shift', 'tab')
-    time.sleep(0.5)
+    # time.sleep(0.1 )
     pg.press('space')
